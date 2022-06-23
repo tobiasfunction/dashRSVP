5ca73253b0b1e54dffc75c6b61002e7d8881801e
Thomas Slater <thomas.a.slater@gmail.com>
2022-06-23 12:26:34 -0400

    refactoring main.js for readability

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 
-document.getElementById("reader-init").addEventListener("click", () => dashRSVP.init(document.getElementById("reader-display")));
-document.getElementById("reader-play").addEventListener("click", () => dashRSVP.play(document.getElementById("reader-text").value));
-document.getElementById("reader-play-2").addEventListener("click", () => dashRSVP.play());
-document.getElementById("reader-pause").addEventListener("click", () => dashRSVP.pause());
+const displayElement = document.getElementById("reader-display");
+const readerText = document.getElementById("reader-text").value;
+
+document
+  .getElementById("reader-init")
+  .addEventListener("click", () => dashRSVP.init(displayElement));
+document
+  .getElementById("reader-play")
+  .addEventListener("click", () => dashRSVP.play(readerText));
+document
+  .getElementById("reader-play-2")
+  .addEventListener("click", () => dashRSVP.play());
+document
+  .getElementById("reader-pause")
+  .addEventListener("click", () => dashRSVP.pause());

fa1bea0c4fc056da9a8d2403623c25035ca4c35c
Tobias Tobak <tobiasfunction@protonmail.com>
2022-06-23 21:00:15 -0400

    Linting

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const displayElement = document.getElementById("reader-display");
 const readerText = document.getElementById("reader-text");
 
@@ -6,31 +5,24 @@ const initButton = document.getElementById("reader-init");
 const playButton = document.getElementById("reader-play");
 const pauseButton = document.getElementById("reader-pause");
 
-initButton
-    .addEventListener("click", function () {
-        dashRSVP.init(displayElement);
-        readerPause()
-    });
+initButton.addEventListener("click", function () {
+  dashRSVP.init(displayElement);
+  readerPause();
+});
 
-playButton
-    .addEventListener("click", async function () {
-        pauseButton
-            .removeAttribute("disabled")
-        playButton
-            .setAttribute("disabled", "true")
-        await dashRSVP.play(readerText.value);
-        readerPause();
-    })
+playButton.addEventListener("click", async function () {
+  pauseButton.removeAttribute("disabled");
+  playButton.setAttribute("disabled", "true");
 
-pauseButton
-    .addEventListener("click", function () {
-        dashRSVP.pause();
-        readerPause();
-    })
+  await dashRSVP.play(readerText.value).then(() => readerPause());
+});
+
+pauseButton.addEventListener("click", function () {
+  dashRSVP.pause();
+  readerPause();
+});
 
 function readerPause() {
-    pauseButton
-        .setAttribute("disabled", "true")
-    playButton
-        .removeAttribute("disabled")
+  pauseButton.setAttribute("disabled", "true");
+  playButton.removeAttribute("disabled");
 }
--- rsvp/rsvp.js
+++ rsvp/rsvp.js
@@ -1,89 +1,107 @@
 const dashRSVP = {
-    playing: false,
-    position: 0,
-    wpm: 200,
-    init: function (readerDisplay, inputText) {
-        if (inputText) setText(inputText);
-        if (!readerDisplay.matches('div')) return console.error("dashRSVP: Initialized object must be a [div]")
+  playing: false,
+  position: 0,
+  wpm: 200,
+  init: function (readerDisplay, inputText) {
+    if (inputText) setText(inputText);
+    if (!readerDisplay.matches("div"))
+      return console.error("dashRSVP: Initialized object must be a [div]");
 
-        this.frame = readerDisplay;
-        if (this.frame.hasChildNodes()) this.frame.innerHTML = "";
-        if (!!this.container) this.container.remove();
-        this.playing = false;
-        this.position = 0;
+    this.frame = readerDisplay;
+    if (this.frame.hasChildNodes()) this.frame.innerHTML = "";
+    if (!!this.container) this.container.remove();
+    this.playing = false;
+    this.position = 0;
 
-        this.container = newDiv("RSVP-container");
+    this.container = newDiv("RSVP-container");
 
-        const topBorder = newDiv("RSVP-border RSVP-border-top");
-        topBorder.append(newDiv("RSVP-left"), newDiv("RSVP-notch"), newDiv("RSVP-right"));
+    const topBorder = newDiv("RSVP-border RSVP-border-top");
+    topBorder.append(
+      newDiv("RSVP-left"),
+      newDiv("RSVP-notch"),
+      newDiv("RSVP-right")
+    );
 
-        const wordContainer = newDiv("RSVP-word");
+    const wordContainer = newDiv("RSVP-word");
 
-        const bottomBorder = newDiv("RSVP-border RSVP-border-bottom");
-        bottomBorder.append(newDiv("RSVP-left"), newDiv("RSVP-notch"), newDiv("RSVP-right"));
+    const bottomBorder = newDiv("RSVP-border RSVP-border-bottom");
+    bottomBorder.append(
+      newDiv("RSVP-left"),
+      newDiv("RSVP-notch"),
+      newDiv("RSVP-right")
+    );
 
+    this.substrings = {
+      left: newDiv("RSVP-left"),
+      mid: newDiv("RSVP-mid"),
+      right: newDiv("RSVP-right"),
+    };
+    wordContainer.append(
+      this.substrings.left,
+      this.substrings.mid,
+      this.substrings.right
+    );
 
-        this.substrings = {
-            left: newDiv("RSVP-left"),
-            mid: newDiv("RSVP-mid"),
-            right: newDiv("RSVP-right")
-        }
-        wordContainer.append(this.substrings.left, this.substrings.mid, this.substrings.right);
+    this.container.append(topBorder, wordContainer, bottomBorder);
+    this.frame.append(this.container);
 
-        this.container.append(topBorder, wordContainer, bottomBorder);
-        this.frame.append(this.container);
+    function newDiv(className) {
+      let tempDiv = document.createElement("div");
+      tempDiv.className = className;
+      return tempDiv;
+    }
+  },
+  setText: function (inputText) {
+    if (typeof (inputText !== "string")) {
+    }
+    this.textString = inputText;
+    this.textArray = this.textString.split(/\s+/gm);
+  },
+  play: async function (inputText) {
+    if (this.playing) return;
+    if (!this.container)
+      return console.error("dashRSVP: Reader has not been initialized.");
+    if (!inputText && !this.textString)
+      return console.log("dashRSVP: No text to display.");
+    if (!!inputText && inputText !== this.textString) {
+      this.setText(inputText);
+      this.position = 0;
+    }
+    this.playing = true;
 
-        function newDiv(className) {
-            let tempDiv = document.createElement("div");
-            tempDiv.className = className;
-            return tempDiv;
-        }
-    },
-    setText: function (inputText) {
-        if (typeof (inputText !== 'string')) {
-        }
-        this.textString = inputText;
-        this.textArray = this.textString.split(/\s+/gm);
-    },
-    play: async function (inputText) {
-        if (this.playing) return;
-        if (!this.container) return console.error("dashRSVP: Reader has not been initialized.")
-        if (!inputText && !this.textString) return console.log("dashRSVP: No text to display.");
-        if (!!inputText && (inputText !== this.textString)) {
-            this.setText(inputText)
-            this.position = 0;
-        }
-        this.playing = true
+    let basePeriod = 60000 / this.wpm; // Convert words-per-minute to milliseconds-per-word
 
-        let basePeriod = 60000 / this.wpm; // Convert words-per-minute to milliseconds-per-word
+    while (this.playing && this.position < this.textArray.length) {
+      const word = this.textArray[this.position];
+      const pivot = Math.ceil((word.length - 1) * 0.25);
 
-        while (this.playing && this.position < this.textArray.length) {
-            const word = this.textArray[this.position];
-            const pivot = Math.ceil((word.length - 1) * .25);
+      this.substrings.left.innerText = word.substring(0, pivot);
+      this.substrings.mid.innerText = word.substring(pivot, pivot + 1);
+      this.substrings.right.innerText = word.substring(pivot + 1);
 
-            this.substrings.left.innerText = word.substring(0, pivot);
-            this.substrings.mid.innerText = word.substring(pivot, pivot + 1);
-            this.substrings.right.innerText = word.substring(pivot + 1);
-
-            await this.wait(basePeriod);
-            this.position++;
-        }
-        if (this.position >= this.textArray.length) {
-            this.playing = false;
-            this.position = 0;
-        }
-    },
-    pause: function () { this.playing = false },
-    restart: function () {
-        this.position = 0;
-        this.playing = false;
-    },
-    reset: function () {
-        this.playing = false;
-        this.textString = null;
-        this.textArray = null;
-        this.position = 0;
-    },
-    remove: function () { this.container.remove(); },
-    wait: ms => new Promise(resolve => setTimeout(resolve, ms))
-}
+      await this.wait(basePeriod);
+      this.position++;
+    }
+    if (this.position >= this.textArray.length) {
+      this.playing = false;
+      this.position = 0;
+    }
+  },
+  pause: function () {
+    this.playing = false;
+  },
+  restart: function () {
+    this.position = 0;
+    this.playing = false;
+  },
+  reset: function () {
+    this.playing = false;
+    this.textString = null;
+    this.textArray = null;
+    this.position = 0;
+  },
+  remove: function () {
+    this.container.remove();
+  },
+  wait: (ms) => new Promise((resolve) => setTimeout(resolve, ms)),
+};
